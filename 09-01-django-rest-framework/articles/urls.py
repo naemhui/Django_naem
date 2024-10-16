9c801e7c275f442f54738001fc4a11d8e821f13f
권남희 <aotm_1245@naver.com>
2024-10-16 11:22:33 +0900

    1016 DRF1 수업

--- 09-01-django-rest-framework/articles/urls.py
+++ 09-01-django-rest-framework/articles/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from articles import views
 
-
+# app_name, url 이름 사라짐 (template 안 쓸 거니까)
 urlpatterns = [
-    # path('articles/', views.article_list),
-    # path('articles/<int:article_pk>/', views.article_detail),
+    path('articles/', views.article_list),
+    path('articles/<int:article_pk>/', views.article_detail),
 ]
