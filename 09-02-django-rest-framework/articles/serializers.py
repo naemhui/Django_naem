99033330a0aa8ebe244ff2a07670b27ff2493ff1
권남희 <knaem.edu@gmail.com>
2024-10-17 11:09:09 +0900

    1017 수업 내용

--- 09-02-django-rest-framework/articles/serializers.py
+++ 09-02-django-rest-framework/articles/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Article
+from .models import Article, Comment
 
 
 class ArticleListSerializer(serializers.ModelSerializer):
@@ -11,8 +11,42 @@ class ArticleListSerializer(serializers.ModelSerializer):
             'content',
         )
 
-
+# 댓글 단일조회 할 때 쓰고 있는 클래스
 class ArticleSerializer(serializers.ModelSerializer):
+    class CommentDerailSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Comment
+            fields = ('id', 'content',)
+
+    # comment_set 역참조 데이터를 override
+    comment_set = CommentDerailSerializer(read_only=True, many=True)
+    # 댓글 개수 제공을 위한 새로운 필드 생성
+    # source 속성: 필드를 채우는 데 사용할 속성의 이름
+    comment_count = serializers.IntegerField(source='comment_set.count', read_only=True)
+
     class Meta:
         model = Article
         fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class ArticletitleSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Article
+            fields = ('title',)
+
+    # 기존 article 데이터 값을 override
+    # 그런데 기존 필드를 override하게 되면 Meta 클래스의 read_only_fields 사용 못함
+    # 모델 시리얼라이저의 read_only인자 값으로 재설정
+    article = ArticletitleSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        # fields에 작성된 필드는 모두 유효성 검사 목록에 포함됨
+        fields = '__all__'
+
+        # 외래키 필드를 읽기전용 필드로 지정
+        # 이유는?
+        # 외래키 데이터는
+        # 유효성 검사에서 제외, 결과 데이터에는 포함하고 싶음
+        read_only_fields = ('article',)
--- 09-02-django-rest-framework/articles/views.py
+++ 09-02-django-rest-framework/articles/views.py
@@ -2,8 +2,9 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 
-from .models import Article
-from .serializers import ArticleListSerializer, ArticleSerializer
+from django.shortcuts import get_list_or_404
+from .models import Article, Comment
+from .serializers import ArticleListSerializer, ArticleSerializer, CommentSerializer
 
 
 @api_view(['GET', 'POST'])
@@ -22,8 +23,8 @@ def article_list(request):
 
 @api_view(['GET', 'DELETE', 'PUT'])
 def article_detail(request, article_pk):
-    article = Article.objects.get(pk=article_pk)
-
+    # article = Article.objects.get(pk=article_pk)
+    article = get_list_or_404(Article, pk=article_pk)
     if request.method == 'GET':
         serializer = ArticleSerializer(article)
         return Response(serializer.data)
@@ -39,3 +40,47 @@ def article_detail(request, article_pk):
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data)
+
+
+
+@api_view(['GET'])
+def comment_list(request):
+    # 댓글 전체 조회
+    comments = Comment.objects.all()
+    # 댓글 목록 쿼리셋을 직렬화(serialization)
+    # 어떤 포맷이든 재가공하기 쉽게 하는 것
+    # 어떤 운영체재나 언어를 쓰는 개발자든 유연하게 사용할 수 있도록
+    serializer = CommentSerializer(comments, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET', 'DELETE', 'PUT'])
+def comment_detail(request, comment_pk):
+    # 단일 대글 조회
+    comment = Comment.objects.get(pk=comment_pk)
+    if request.method == 'GET':
+        # 단일 댓글 직렬화
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    elif request.method == 'PUT':
+        serializer = CommentSerializer(comment, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)    
+
+
+@api_view(['POST'])
+def comment_create(request, article_pk):
+    # 게시글 조회 (어떤 게시글에 작성되는 댓글인지)
+    article = Article.objects.get(pk=article_pk)
+    # 사용자 입력 데이터를 직렬화 (사용자가 입력한 댓글 데이터) -> 외래키 없음
+    serializer = CommentSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        # 외래키 데이터 입력 후 저장
+        # article(serializers.py의 필드명) = article(위의 변수명)
+        serializer.save(article=article)  # drf의 modelserializer에서는 이렇게 넣음
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
